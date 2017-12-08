3f49c90f7a9a64ca66ea6640a8e3169fb572be96
r-i-c-h <rich@richwerden.com>
2017-12-08 00:57:26 -0800

    cleans up mainjs a bit

--- js/main.js
+++ js/main.js
@@ -3,22 +3,19 @@ layout: null
 sitemap:
   exclude: 'yes'
 ---
-/* I didn't put the jQuery in there, it came that way. I'm afraid to remove it lest it break something...*/
-/* ('.btn-mobile-menu') ('.btn-mobile-menu__icon').toggle ('.content-wrapper') ('.panel-cover') ('a.blog-button') */
 
-// Blog Button is #blogbutton        Proj Button is #projbutton
-  $(document).ready(function () {
+/* I didn't put the jQuery in there, it came that way. I'm afraid to remove it lest it break something... */
 
+$(document).ready(function () {
   if (window.location.hash && window.location.hash == '#blog') {
     $('.panel-cover').addClass('panel-cover--collapsed')
   }
 
-  if (window.location.pathname !== '{{ site.baseurl }}/' && window.location.pathname !== '{{ site.baseurl }}/index.html') {
-    $('.panel-cover').addClass('panel-cover--collapsed');
+  if ( $('.panel-cover').width() < 960 ) {
+    $('.panel-cover').addClass('panel-cover--collapsed')
   }
 
-  if (( $('.panel-cover').width() < 960 ) || ( $('.panel-cover').hasClass('panel-cover--collapsed') )) {
-    $('.panel-cover').addClass('panel-cover--collapsed') 
+  if ( $('.panel-cover').hasClass('panel-cover--collapsed') ) {
     $('.panel-cover__description').remove()
     $('.panel-cover__divider--secondary').remove()
   }

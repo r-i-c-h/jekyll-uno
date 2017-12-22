b191aef85f4eaa38340e2085050c64e5fb19b04a
r-i-c-h <rich@richwerden.com>
2017-12-21 19:12:32 -0800

    Mostly meaningless indentation changes and a #id swap

--- js/main.js
+++ js/main.js
@@ -3,57 +3,59 @@ layout: null
 sitemap:
   exclude: 'yes'
 ---
-
-const panel = document.getElementById('magic-panel')
-const hasCollapsed = () => { return panel.classList.contains('panel-cover--collapsed') }
+const panel = document.getElementById('grey-panel')
+const isPanelCollapsed = () => { return panel.classList.contains('panel-cover--collapsed') }
 const addCollapsed = () => { panel.classList.add('panel-cover--collapsed') }
-const shrinkPanel = () => {
-  document.getElementsByClassName('navigation-wrapper')[0].style.flexDirection = 'column'
-  document.getElementsByClassName('panel-cover__description')[0].style.display = 'none'
-  document.getElementsByClassName('panel-cover__divider--secondary')[0].style.display = 'none'
-  panel.style.width = '25%' // Animation via CSS Transition
+
+if (window.location.hash && window.location.hash == '#blog') { 
+  addCollapsed() 
 }
-
-const mainContent = document.getElementsByClassName('content-wrapper')[0]
-const mobileTopBar = document.getElementById('mobile-menu-topbar')
-
-if (window.location.hash && window.location.hash == '#blog') {
-  addCollapsed()
-}
-
 if (window.location.pathname !== '{{ site.baseurl }}/' && window.location.pathname !== '{{ site.baseurl }}/index.html') {
   addCollapsed()
 }
 
+const bigButtons = document.getElementsByClassName('big-button')
+const blogBtn = document.getElementById('blog-btn')
 const projectBtn = document.getElementById('project-btn')
-projectBtn.addEventListener('click', (e) => { e.preventDefault() })
 const loadProjectPage = () => { window.location.href = '{{ site.baseurl }}/projects/' }
 
-const bigButtons = document.getElementsByClassName('big-button')
+projectBtn.addEventListener('click', (e) => { e.preventDefault() })
 for (let i = 0 ; i < bigButtons.length; i++) {
-  bigButtons[i].addEventListener('click', (e) => {
-    if ( hasCollapsed() && (e.target === projectBtn) ) loadProjectPage()
-    if ( hasCollapsed() ) return
-
+  bigButtons[i].addEventListener('click', (e) => {   
+    const mainContent = document.getElementsByClassName('content-wrapper')[0]
+ 
+    const checkForProjectPageChange = () => {
+      if (e.target === projectBtn) loadProjectPage()
+      return null;
+    }
+    
+    if ( isPanelCollapsed() ) return checkForProjectPageChange()
+ 
     if ( panel.clientWidth < 960 ) {
       addCollapsed()
-    } else { 
-        // if project button, change window.location after panel is done with transition
-        if ( e.target === projectBtn ) {
-          mainContent.style.display = 'none'
-          panel.addEventListener('transitionend', (e) => {
-            loadProjectPage()
-          }, true)
-        }
-        shrinkPanel()
-      }
-      if ( e.target === document.getElementById('blog-btn') ) {
-        mainContent.style.display = 'block'
-        mainContent.classList.add('animated') // $('.content-wrapper').addClass('animated slideInRight')
-        mainContent.classList.add('slideInRight')
-      }
+      return checkForProjectPageChange();
+    }
+    
+    if ( e.target === projectBtn ) { // Setup project page reload for *after* transitions ends
+      mainContent.style.display = 'none'
+      panel.addEventListener('transitionend', (e) => {
+        loadProjectPage()
+      })
+    }
+      
+    /* Shrink Panel To The Left */
+    document.getElementsByClassName('navigation-wrapper')[0].style.flexDirection = 'column'
+    document.getElementsByClassName('panel-cover__description')[0].style.display = 'none'
+    document.getElementsByClassName('panel-cover__divider--secondary')[0].style.display = 'none'
+    panel.style.width = '25%' // Animation via CSS Transition
+  
+    if ( e.target === blogBtn ) { // Main blog content fly in from left animation 
+      mainContent.style.display = 'block'
+      mainContent.classList.add('animated')
+      mainContent.classList.add('slideInRight')
+    }
   })
-} // endFor 
+}
 
   
 /*   $('.btn-mobile-menu').click(function () {

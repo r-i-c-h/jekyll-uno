3ac80c9e48549f90b93218892516bc2b6cca338d
r-i-c-h <rich@richwerden.com>
2017-12-24 01:50:02 -0800

    bugfix: page not changing on prjbtn click after blgbtn click

--- js/main.js
+++ js/main.js
@@ -7,9 +7,6 @@ const panel = document.getElementById('grey-panel')
 const isPanelCollapsed = () => { return panel.classList.contains('panel-cover--collapsed') }
 const addCollapsed = () => { panel.classList.add('panel-cover--collapsed') }
 
-if (window.location.hash && window.location.hash == '#blog') { 
-  addCollapsed() 
-}
 if (window.location.pathname !== '{{ site.baseurl }}/' && window.location.pathname !== '{{ site.baseurl }}/index.html') {
   addCollapsed()
 }
@@ -19,13 +16,15 @@ const blogBtn = document.getElementById('blog-btn')
 const projectBtn = document.getElementById('project-btn')
 const loadProjectPage = () => { window.location.href = '{{ site.baseurl }}/projects/' }
 
+/*** CLICK HANDLERS ***/
 projectBtn.addEventListener('click', (e) => { e.preventDefault() })
-for (let i = 0 ; i < bigButtons.length; i++) {
+for (let i = 0; i < bigButtons.length; i++) {
   bigButtons[i].addEventListener('click', (e) => {   
+    debugger;
     const mainContent = document.getElementsByClassName('content-wrapper')[0]
  
     const checkForProjectPageChange = () => {
-      if (e.target === projectBtn) loadProjectPage()
+      if (e.currentTarget === projectBtn) loadProjectPage()
       return null;
     }
     
@@ -36,7 +35,7 @@ for (let i = 0 ; i < bigButtons.length; i++) {
       return checkForProjectPageChange();
     }
     
-    if ( e.target === projectBtn ) { // Setup project page reload for *after* transitions ends
+    if ( e.currentTarget === projectBtn ) { // Setup project page reload for *after* transitions ends
       mainContent.style.display = 'none'
       panel.addEventListener('transitionend', (e) => {
         loadProjectPage()
@@ -49,7 +48,7 @@ for (let i = 0 ; i < bigButtons.length; i++) {
     document.getElementsByClassName('panel-cover__divider--secondary')[0].style.display = 'none'
     panel.style.width = '25%' // Animation via CSS Transition
   
-    if ( e.target === blogBtn ) { // Main blog content fly in from left animation 
+    if ( e.currentTarget === blogBtn ) { // Main blog content fly in from left animation 
       mainContent.style.display = 'block'
       mainContent.classList.add('animated')
       mainContent.classList.add('slideInRight')
@@ -57,7 +56,6 @@ for (let i = 0 ; i < bigButtons.length; i++) {
   })
 }
 
-  
 /*   $('.btn-mobile-menu').click(function () {
     $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
     $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')

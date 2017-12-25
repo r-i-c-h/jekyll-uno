fe4475ace305e7d34a0966e833dd942bceba8176
r-i-c-h <rich@richwerden.com>
2017-12-25 12:58:53 -0800

    JS fixes= Mobile+Desktop UI/UX OK

--- js/main.js
+++ js/main.js
@@ -1,68 +1,114 @@
 ---
 layout: null
 sitemap:
-  exclude: 'yes'
+exclude: 'yes'
 ---
+// debugger;
 const panel = document.getElementById('grey-panel')
-const isPanelCollapsed = () => { return panel.classList.contains('panel-cover--collapsed') }
 const addCollapsed = () => { panel.classList.add('panel-cover--collapsed') }
 
 if (window.location.pathname !== '{{ site.baseurl }}/' && window.location.pathname !== '{{ site.baseurl }}/index.html') {
   addCollapsed()
+} 
+
+if (window.location.hash && window.location.hash == '#blog') {
+  addCollapsed()
+}
+
+const flipMobileTopSpanIcons = () => {
+  const topSpanIcon = document.getElementsByClassName('btn-mobile-menu__icon')[0];
+  topSpanIcon.classList.add('fadeOut')
+  topSpanIcon.classList.toggle('icon-list')
+  topSpanIcon.classList.toggle('icon-x-circle')
+  topSpanIcon.classList.remove('fadeOut')
+}
+
+const toggleMobileNavDisplay = (cbFunc) => {
+  flipMobileTopSpanIcons()
+  const navWrapper = document.getElementsByClassName('navigation-wrapper')[0]
+  if (navWrapper.classList.contains('visible')) {
+    Promise.resolve() // Promises used for sequential triggering
+      .then( _ => navWrapper.classList.toggle('bounceOutUp') )
+      .then( _ => navWrapper.classList.toggle('bounceInDown') )
+      .then( _ => { if (cbFunc) { cbFunc() } } )
+      .catch( err => console.error(err) )
+  } else {
+    navWrapper.classList.add('animated')
+    navWrapper.classList.add('bounceInDown')
+    navWrapper.classList.add('visible')
+  }
 }
 
 const bigButtons = document.getElementsByClassName('big-button')
-const blogBtn = document.getElementById('blog-btn')
 const projectBtn = document.getElementById('project-btn')
 const loadProjectPage = () => { window.location.href = '{{ site.baseurl }}/projects/' }
 
-/*** CLICK HANDLERS ***/
+/** Click Handlers **/
 projectBtn.addEventListener('click', (e) => { e.preventDefault() })
+
+const mobileTopSpan = document.getElementById('mobile-menu-topbar') // aka .mobile and .btn-mobile-menu
+mobileTopSpan.addEventListener('click', (e) => { toggleMobileNavDisplay() })
+
 for (let i = 0; i < bigButtons.length; i++) {
-  bigButtons[i].addEventListener('click', (e) => {   
-    debugger;
-    const mainContent = document.getElementsByClassName('content-wrapper')[0]
- 
-    const checkForProjectPageChange = () => {
-      if (e.currentTarget === projectBtn) loadProjectPage()
-      return null;
+  
+  bigButtons[i].addEventListener('click', (e) => {
+    const isMobileEnv = () => { 
+      const navWrapper = document.getElementsByClassName('navigation-wrapper')[0]
+      return navWrapper.classList.contains('bounceInDown')
     }
-    
-    if ( isPanelCollapsed() ) return checkForProjectPageChange()
- 
-    if ( panel.clientWidth < 960 ) {
-      addCollapsed()
-      return checkForProjectPageChange();
+    const checkForProjBtnClicked = () => {
+      if (e.currentTarget === projectBtn) { 
+        return isMobileEnv() ? toggleMobileNavDisplay(loadProjectPage) : loadProjectPage()
+      }
+      if (isMobileEnv()) { toggleMobileNavDisplay() }
     }
-    
-    if ( e.currentTarget === projectBtn ) { // Setup project page reload for *after* transitions ends
-      mainContent.style.display = 'none'
-      panel.addEventListener('transitionend', (e) => {
-        loadProjectPage()
-      })
+
+    if ( panel.classList.contains('panel-cover--collapsed') ) return checkForProjBtnClicked()
+    if ( panel.clientWidth <= 960 ) { 
+      addCollapsed() 
+      return checkForProjBtnClicked()
     }
-      
-    /* Shrink Panel To The Left */
+
     document.getElementsByClassName('navigation-wrapper')[0].style.flexDirection = 'column'
     document.getElementsByClassName('panel-cover__description')[0].style.display = 'none'
     document.getElementsByClassName('panel-cover__divider--secondary')[0].style.display = 'none'
-    panel.style.width = '25%' // Animation via CSS Transition
+    if ( isMobileEnv() ) {
+      if (e.currentTarget === projectBtn) { return toggleMobileNavDisplay(loadProjectPage) }
+      if (e.currentTarget === document.getElementById('blog-btn')) { return toggleMobileNavDisplay() }
+    }
+
+    const mainContent = document.getElementsByClassName('content-wrapper')[0]
   
-    if ( e.currentTarget === blogBtn ) { // Main blog content fly in from left animation 
+    if (e.currentTarget === document.getElementById('blog-btn')) { 
       mainContent.style.display = 'block'
+      panel.style.width = '25%' /* Animation via CSS Transition */
       mainContent.classList.add('animated')
       mainContent.classList.add('slideInRight')
+      return
+    }
+
+    if (e.currentTarget === projectBtn) { 
+      const whichEndEventName = () => {
+        const el = document.createElement('fakeelement');
+        const transitionEndPrefixes = {
+          'transition':'transitionend',
+          'OTransition':'oTransitionEnd',
+          'MozTransition':'transitionend',
+          'WebkitTransition':'webkitTransitionEnd'
+        }
+        let nm;
+        for ( nm in transitionEndPrefixes ){
+          if( el.style[nm] !== undefined ){
+            return transitionEndPrefixes[nm];
+          }
+        }
+      }
+
+      mainContent.style.display = 'none'
+      const endOfTransition = whichEndEventName();
+      panel.addEventListener( endOfTransition, (e) => { loadProjectPage() });
+      panel.style.width = '25%' /* Animation via CSS Transition */
     }
   })
-}
 
-/*   $('.btn-mobile-menu').click(function () {
-    $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
-    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
-  })
- */
-/*  $('.navigation-wrapper .blog-button').click(function () {
-    $('.navigation-wrapper').toggleClass('visible')
-    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
-  })
- */
+}
